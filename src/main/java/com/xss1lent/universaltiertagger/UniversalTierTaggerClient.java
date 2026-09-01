60c0b272510f3376755c3b15461e6074882e0a2e
prolo292 <tobiaslaruso@gmail.com>
2026-09-01 23:47:30 +0100

    Initialize configuration, cache, and providers
    Added configuration, cache, and provider manager initialization.

--- src/main/java/com/xss1lent/universaltiertagger/UniversalTierTaggerClient.java
+++ src/main/java/com/xss1lent/universaltiertagger/UniversalTierTaggerClient.java
@@ -1,5 +1,8 @@
 package com.xss1lent.universaltiertagger;
 
+import com.xss1lent.universaltiertagger.cache.TierCache;
+import com.xss1lent.universaltiertagger.config.TierTaggerConfig;
+import com.xss1lent.universaltiertagger.provider.TierProviderManager;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,8 +12,18 @@ public class UniversalTierTaggerClient implements ClientModInitializer {
     public static final String MOD_ID = "universal_tiertagger";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static TierTaggerConfig CONFIG;
+    public static TierCache CACHE;
+    public static TierProviderManager PROVIDERS;
+
     @Override
     public void onInitializeClient() {
-        LOGGER.info("Universal TierTagger has been initialized!");
+        LOGGER.info("Initializing Universal TierTagger...");
+
+        CONFIG = TierTaggerConfig.load();
+        CACHE = new TierCache();
+        PROVIDERS = new TierProviderManager();
+
+        LOGGER.info("Universal TierTagger initialized!");
     }
 }
