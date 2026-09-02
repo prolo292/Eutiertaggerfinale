99ed0c1084e2fbf8f45c51eb2d09b4ef86762651
prolo292 <tobiaslaruso@gmail.com>
2026-09-02 01:26:47 +0100

    Refactor key binding to use KeyMapping

--- src/main/java/com/xss1lent/universaltiertagger/keybind/KeyBindings.java
+++ src/main/java/com/xss1lent/universaltiertagger/keybind/KeyBindings.java
@@ -1,28 +1,29 @@
 package com.xss1lent.universaltiertagger.keybind;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.xss1lent.universaltiertagger.gui.TierTaggerScreen;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.KeyMapping;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
 
-    public static KeyBinding OPEN_MENU;
+    public static KeyMapping OPEN_MENU;
 
     public static void register() {
+
         OPEN_MENU = KeyBindingHelper.registerKeyBinding(
-                new KeyBinding(
+                new KeyMapping(
                         "key.universal_tiertagger.open_menu",
-                        InputUtil.Type.KEYSYM,
+                        InputConstants.Type.KEYSYM,
                         GLFW.GLFW_KEY_Y,
                         "category.universal_tiertagger"
                 )
         );
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (OPEN_MENU.wasPressed()) {
+            while (OPEN_MENU.consumeClick()) {
                 client.setScreen(new TierTaggerScreen());
             }
         });
