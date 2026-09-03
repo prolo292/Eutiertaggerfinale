3ddad65304551cde87da138c548eaf44f5e26fb8
prolo292 <tobiaslaruso@gmail.com>
2026-09-03 20:51:26 +0100

    Implement PlayerInfoAccessor in PlayerListEntryMixin

--- src/main/java/com/xss1lent/universaltiertagger/mixin/PlayerListEntryMixin.java
+++ src/main/java/com/xss1lent/universaltiertagger/mixin/PlayerListEntryMixin.java
@@ -6,11 +6,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(PlayerInfo.class)
-public abstract class PlayerListEntryMixin {
+public abstract class PlayerListEntryMixin
+        implements PlayerInfoAccessor {
 
     @Shadow
     public abstract GameProfile getProfile();
 
+    @Override
     public String universalTierTagger$getUsername() {
 
         GameProfile profile = getProfile();
