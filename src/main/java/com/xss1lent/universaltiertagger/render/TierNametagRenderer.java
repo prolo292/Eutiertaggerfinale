4d26f75dba2d05306c9fbc8de620af5eb9fd20f2
prolo292 <tobiaslaruso@gmail.com>
2026-09-03 20:35:20 +0100

    Refactor nametag rendering methods for clarity

--- src/main/java/com/xss1lent/universaltiertagger/render/TierNametagRenderer.java
+++ src/main/java/com/xss1lent/universaltiertagger/render/TierNametagRenderer.java
@@ -1,20 +1,36 @@
 package com.xss1lent.universaltiertagger.render;
 
 import com.xss1lent.universaltiertagger.UniversalTierTaggerClient;
+import com.xss1lent.universaltiertagger.display.TierDisplayManager;
 import com.xss1lent.universaltiertagger.display.TierTextFormatter;
 
 public class TierNametagRenderer {
 
-    public static String getSecondaryNametag(String username) {
+    public static String getPrimaryText(String username) {
 
         if (UniversalTierTaggerClient.CONFIG == null) {
             return "";
         }
 
-        if (!UniversalTierTaggerClient.CONFIG.showSecondary) {
+        TierDisplayManager.DisplayTier tier =
+                TierDisplayManager.getPrimaryTier(username);
+
+        return TierTextFormatter.formatPrimary(tier);
+    }
+
+    public static String getSecondaryText(String username) {
+
+        if (UniversalTierTaggerClient.CONFIG == null) {
             return "";
         }
 
-        return TierTextFormatter.getSecondaryText(username);
+        if (!UniversalTierTaggerClient.CONFIG.showSecondaryTierlist) {
+            return "";
+        }
+
+        TierDisplayManager.DisplayTier tier =
+                TierDisplayManager.getSecondaryTier(username);
+
+        return TierTextFormatter.formatSecondary(tier);
     }
 }
