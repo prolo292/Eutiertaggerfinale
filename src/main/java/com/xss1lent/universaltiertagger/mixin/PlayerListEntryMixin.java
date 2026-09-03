890f52885d38a5a3eb09df5ea2d1ed2fdf5b39c1
prolo292 <tobiaslaruso@gmail.com>
2026-09-03 19:56:47 +0100

    Add PlayerListEntryMixin for PlayerInfo class

--- src/main/java/com/xss1lent/universaltiertagger/mixin/PlayerListEntryMixin.java
+++ src/main/java/com/xss1lent/universaltiertagger/mixin/PlayerListEntryMixin.java
@@ -1,1 +1,13 @@
+package com.xss1lent.universaltiertagger.mixin;
 
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.multiplayer.PlayerInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(PlayerInfo.class)
+public abstract class PlayerListEntryMixin {
+
+    @Shadow
+    public abstract GameProfile getProfile();
+}
