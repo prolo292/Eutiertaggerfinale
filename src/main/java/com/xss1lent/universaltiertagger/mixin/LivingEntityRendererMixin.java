4937182741424ecaa9d688d004d382c0dc47f134
prolo292 <tobiaslaruso@gmail.com>
2026-09-08 14:06:40 +0100

    Add mixin for LivingEntityRenderer class

--- src/main/java/com/xss1lent/universaltiertagger/mixin/LivingEntityRendererMixin.java
+++ src/main/java/com/xss1lent/universaltiertagger/mixin/LivingEntityRendererMixin.java
@@ -1,1 +1,8 @@
+package com.xss1lent.universaltiertagger.mixin;
 
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(LivingEntityRenderer.class)
+public abstract class LivingEntityRendererMixin {
+}
