a05159f20849f08f276ad480c18886a1f1f89153
prolo292 <tobiaslaruso@gmail.com>
2026-09-09 06:26:28 +0100

    Refactor PlayerListEntryMixin class structure

--- src/main/java/com/xss1lent/universaltiertagger/mixin/PlayerListEntryMixin.java
+++ src/main/java/com/xss1lent/universaltiertagger/mixin/PlayerListEntryMixin.java
@@ -6,15 +6,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(PlayerInfo.class)
-public abstract class PlayerListEntryMixin
-        implements PlayerInfoAccessor {
+public abstract class PlayerListEntryMixin {
 
     @Shadow
     public abstract GameProfile getProfile();
 
-    @Override
     public String universalTierTagger$getUsername() {
-
         GameProfile profile = getProfile();
 
         if (profile == null) {
@@ -22,7 +19,6 @@ public abstract class PlayerListEntryMixin
         }
 
         String name = profile.name();
-
         return name != null ? name : "";
     }
 }
